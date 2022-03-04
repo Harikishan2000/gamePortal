0153780e50c0419a2c9e9bfe6fbcc03a1c2dff2a
sangeeta <singhharikishanjnv2011@gmail.com>
2022-03-04 09:11:00 +0530

    1.2

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 const express = require("express");
-const bodyParser = require("body-parser");
-const mongoose = require('mongoose');  
+const bodyParser = require("body-parser"); 
 const  ejs =require("ejs");
 
  var user;
